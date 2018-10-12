1bc229a13897eac0f274c5b0e5589e3ae9e6dfee
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-12 22:29:51 +0530

    removed extra files

--- demo/cepheus/cepheus_oneclick.sh
+++
@@ -1,5 +0,0 @@
-kubectl create -f   ./cepheus_svc.yaml
-kubectl create -f  ./cepheus_rc.yaml
-echo "===================waiting for cepheus to come up ==================="
-sleep 35
-echo "========================= cepheus deploy completed ============================"
