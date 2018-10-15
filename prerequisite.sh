68d0de22f54fb13e00e381cd7e26d6e8413bb309
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-15 11:49:46 +0530

    prerequisite.sh

---
+++ prerequisite.sh
@@ -0,0 +1,8 @@
+yum install python-pip -y
+pip install ansible
+pip install shade
+echo "+++++++++++++++please source your openstack auth/rc file here+++++++++++++"
+sed -i '/host_key_checking = False/s/^#//g' /etc/ansible/ansible.cfg
+echo "+++++++++++++ we are done with prerequisite ++++++++++++"
+echo "++++++++ please edit cloud-config.yaml file for inputs and run this command    'ansible-playbook site.yaml'     ++++++++++ "
+
