d446c076b198b893c0457a336ce8bbabcd53061b
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-15 19:19:43 +0530

    created volumes for orion-mongodb

--- IoTGEs-templates/orion/orion_one_click.sh
+++ IoTGEs-templates/orion/orion_one_click.sh
@@ -1,7 +1,9 @@
 echo "++++++++++orion cleanup done+++++++++++"
 mkdir -p  /opt/data/vol/{0..2}
 kubectl create  configmap wrapper --from-file=./kube-mongo-statefulset.sh
-kubectl create  -f ./volume.yaml
+kubectl create  -f ./volume1.yaml
+kubectl create  -f ./volume2.yaml
+kubectl create  -f ./volume3.yaml
 kubectl create  -f ./kube_mongo_client_svc.yaml
 kubectl create -f ./kube_svc_orion.yaml
 sleep 10
--- prerequisite.sh
+++ prerequisite.sh
@@ -5,6 +5,10 @@ pip install -U ansible
 pip install shade
 echo "+++++++++++++++please source your openstack auth/rc file here+++++++++++++"
 sed -i '/host_key_checking = False/s/^#//g' /etc/ansible/ansible.cfg
+sed -i '/pipelining = False/s/^#//g' /etc/ansible/ansible.cfg
+sed -i 's/pipelining = False/pipelining = True/g' /etc/ansible/ansible.cfg
+sed -i '/forks=5/s/^#//g' /etc/ansible/ansible.cfg
+sed -i 's/forks=5/forks=100/g' /etc/ansible/ansible.cfg
 echo "+++++++++++++ we are done with prerequisite ++++++++++++"
 echo "++++++++ please edit cloud-config.yaml file for inputs and run this command    'ansible-playbook site.yaml'     ++++++++++ "
 
