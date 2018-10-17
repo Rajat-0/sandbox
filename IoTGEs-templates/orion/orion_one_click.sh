4bdeed7eb26e7324822a7e8475d68fd2012e762d
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-17 10:22:53 +0530

    some changes regarding dashboard_rc.txt creation for user

--- IoTGEs-templates/orion/orion_one_click.sh
+++ IoTGEs-templates/orion/orion_one_click.sh
@@ -8,12 +8,12 @@ kubectl create  -f ./kube_mongo_client_svc.yaml
 kubectl create -f ./kube_svc_orion.yaml
 sleep 10
 kubectl create -f ./mongo-statefulset.yaml
-echo "======waiting 45 sec for statefulset to deploy===="
-sleep 30
+echo "======waiting 100 sec for statefulset to deploy===="
+sleep 100
 echo "setting up mongodb in HA"
 kubectl	exec  -it mongo-0 /opt/kube-mongo-statefulset.sh
-echo "waiting for HA to complete"
-sleep 20
+echo "waiting 30 sec for HA to complete"
+sleep 30
 echo "waiting for HA to complete"
 echo "waiting for HA to complete 20 sec"
 sleep 10
