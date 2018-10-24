ceabdabea2df98211c02bfdabac33bba3bdc7486
tejinderkhattra <43575793+tejinderkhattra@users.noreply.github.com>
2018-10-24 22:00:13 +0530

    added fogflow-usecases and fogflow config files.

---
+++ fogflow_usecases/topology/TemperatureCheck/st_requirement_update.sh
@@ -0,0 +1,8 @@
+echo "$1"
+echo "$2"
+echo "$3"
+echo "$4"
+echo "$5"
+
+
+curl -iX POST 'http://'$1':30389/ngsi10/updateContext' -H 'Content-Type: application/json' -d '{"contextElements": [{"entityId": {"id": '$2', "type": "Requirement", "isPattern": false}, "attributes": [{"name": "output", "type": "string", "contextValue": "TemperatureAlarm"},{"name": "scheduler", "type": "string", "contextValue": "default"}, {"name": "restriction", "type": "object", "contextValue": {"scopes": [{"scopeType": "circle", "scopeValue": {"centerLatitude": '$3', "centerLongitude": '$4', "radius": '$5'}}]}}], "domainMetadata": [{"name": "topology", "type": "string", "value":"Topology.TemperatureCheck"}]}], "updateAction": "UPDATE"}'
