d196a49b49594c2a445a526d706647a3f89cf4d3
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-27 09:49:47 +0530

    changed start to restart in nfs service

--- IoTGEs-templates/orion/nfs-master.sh
+++ IoTGEs-templates/orion/nfs-master.sh
@@ -5,8 +5,8 @@ chmod 777 /opt/data
 echo "/opt/data *(rw,sync,no_root_squash,no_all_squash)"  >> /etc/exports
 systemctl enable --now rpcbind
 systemctl enable --now nfs-server
-systemctl start rpcbind
-systemctl start nfs-server
+systemctl restart rpcbind
+systemctl restart nfs-server
 mkdir -p /opt/data/vol/0
 mkdir -p /opt/data/vol/1
 mkdir -p /opt/data/vol/2
