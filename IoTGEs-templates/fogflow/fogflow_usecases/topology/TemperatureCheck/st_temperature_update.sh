2cda0caf277cd048d4a9397a701bfcbfae880194
tejinderkhattra <43575793+tejinderkhattra@users.noreply.github.com>
2018-10-28 12:38:43 +0530

    updated st_temperature_update.sh

--- IoTGEs-templates/fogflow/fogflow_usecases/topology/TemperatureCheck/st_temperature_update.sh
+++ IoTGEs-templates/fogflow/fogflow_usecases/topology/TemperatureCheck/st_temperature_update.sh
@@ -7,4 +7,4 @@ echo "$6"
 echo "$7"
 
 
-curl -iX POST 'http://'$1':8080/ngsi10/updateContext' -H 'Content-Type: application/json' -d '{"contextElements": [{"entityId": {"id": '$2', "type": "Temperature", "isPattern": false}, "attributes": [{"name": "currentTemperature", "type": "float", "contextValue": '$3'}, {"name": "deviceId", "type": "string", "contextValue": '$4'}], "domainMetadata": [{"name": "floor", "type": "string", "value": '$5'}, {"name": "location", "type": "point", "value": {"latitude": '$6', "longitude": '$7'}}]}], "updateAction": "UPDATE"}'
+curl -iX POST 'http://'$1':30389/ngsi10/updateContext' -H 'Content-Type: application/json' -d '{"contextElements": [{"entityId": {"id": '$2', "type": "Temperature", "isPattern": false}, "attributes": [{"name": "currentTemperature", "type": "float", "contextValue": '$3'}, {"name": "deviceId", "type": "string", "contextValue": '$4'}], "domainMetadata": [{"name": "floor", "type": "string", "value": '$5'}, {"name": "location", "type": "point", "value": {"latitude": '$6', "longitude": '$7'}}]}], "updateAction": "UPDATE"}'
