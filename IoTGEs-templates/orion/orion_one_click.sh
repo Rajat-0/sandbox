3ddb0560e3af9fa4cb04c816b040b035c1da7ef4
Ravinder Kumar <rhcayadav@gmail.com>
2018-10-30 12:59:57 +0530

    increased time

--- IoTGEs-templates/orion/orion_one_click.sh
+++ IoTGEs-templates/orion/orion_one_click.sh
@@ -6,18 +6,18 @@ kubectl create  -f ./volume2.yaml
 kubectl create  -f ./volume3.yaml
 kubectl create  -f ./kube_mongo_client_svc.yaml
 kubectl create -f ./kube_svc_orion.yaml
-sleep 10
+sleep 50
 kubectl create -f ./mongo-statefulset.yaml
 echo "======waiting 100 sec for statefulset to deploy===="
-sleep 100
+sleep 120
 echo "setting up mongodb in HA"
 kubectl	exec  -it mongo-0 /opt/kube-mongo-statefulset.sh
 echo "waiting 30 sec for HA to complete"
-sleep 30
+sleep 50
 echo "waiting for HA to complete"
 echo "waiting for HA to complete 20 sec"
-sleep 10
+sleep 30
 kubectl create -f ./kube_orion_depoyment.yaml
-sleep 10
+sleep 30
 echo "++++++++++orion oneclick soln completed+++++++++++"
 
